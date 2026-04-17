17bf2cce24f0e1e7990b2e466dd124ff2481188a
nemanjam <nemanja.mitic.elfak@hotmail.com>
2026-04-17 11:20:44 +0200

    redirect in login and register

--- frontend/apps/web/src/app/register/page.tsx
+++ frontend/apps/web/src/app/register/page.tsx
@@ -1,26 +1,39 @@
+import { redirect } from 'next/navigation';
+
 import { Card, CardContent, CardHeader } from '@workspace/ui/components/ui/card';
 
 import FormRegister from '@/components/auth/form-register';
+import { UsersService } from '@/client/sdk.gen';
+import { ROUTES } from '@/constants/routes';
 
 import type { FC } from 'react';
 
-const RegisterPage: FC = () => (
-  <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-slate-50 to-slate-100 dark:from-slate-900 dark:to-slate-800">
-    {/* Card */}
-    <Card className="w-full max-w-md shadow-xl border-0 bg-white/80 backdrop-blur-sm dark:bg-slate-900/80">
-      <CardHeader className="flex-row items-center justify-center mb-6">
-        <div className="w-12 h-12 bg-teal-600 rounded-full flex items-center justify-center mb-0">
-          <div className="w-8 h-8 bg-white rounded-full flex items-center justify-center">
-            <div className="w-4 h-4 bg-teal-600 rounded-full"></div>
+const { DASHBOARD } = ROUTES;
+
+const RegisterPage: FC = async () => {
+  const result = await UsersService.readUserMe();
+  const currentUser = result.data;
+
+  if (currentUser) redirect(DASHBOARD);
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-slate-50 to-slate-100 dark:from-slate-900 dark:to-slate-800">
+      {/* Card */}
+      <Card className="w-full max-w-md shadow-xl border-0 bg-white/80 backdrop-blur-sm dark:bg-slate-900/80">
+        <CardHeader className="flex-row items-center justify-center mb-6">
+          <div className="w-12 h-12 bg-teal-600 rounded-full flex items-center justify-center mb-0">
+            <div className="w-8 h-8 bg-white rounded-full flex items-center justify-center">
+              <div className="w-4 h-4 bg-teal-600 rounded-full"></div>
+            </div>
           </div>
-        </div>
-        <span className="ml-3 text-2xl font-bold text-teal-600">FastAPI</span>
-      </CardHeader>
-      <CardContent>
-        <FormRegister />
-      </CardContent>
-    </Card>
-  </div>
-);
+          <span className="ml-3 text-2xl font-bold text-teal-600">FastAPI</span>
+        </CardHeader>
+        <CardContent>
+          <FormRegister />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
 
 export default RegisterPage;
